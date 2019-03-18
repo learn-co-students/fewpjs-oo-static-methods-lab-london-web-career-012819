2e1031f51e12efebcb0495a02ce1a2aa925e2ac4
SofiaFredriksson <sofiapkf@gmail.com>
2019-03-18 09:25:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let cap = string[0].toUpperCase()
+    return cap + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string) {
+  let exceptedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+  let result = []
+  let arr = string.split(' ')
+  for (let i = 0; i < arr.length; i++) {
+    if (i == 0) {
+      result.push(this.capitalize(arr[i]))
+    } else {
+      if (exceptedWords.includes(arr[i])) {
+        result.push(arr[i])
+      } else {
+        result.push(this.capitalize(arr[i]))
+      }
+    }
+  }
+  return result.join(' ')
+  }
 }
